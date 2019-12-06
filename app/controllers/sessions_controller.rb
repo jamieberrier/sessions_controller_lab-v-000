094a64df0b0f2fe080fcf8666c7dce0432af6348
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 20:17:44 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   def destroy
     if !session[:name].nil?
       session.delete :name
-      render root_path
+      redirect_to root_path
     end
   end
 end
