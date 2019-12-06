20cf5b095861f34ec25ddcfbeebcdbee6ab988ef
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 20:01:21 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       redirect_to sessions_new_path
     else
       session[:name] = params[:name]
-      redirect_to "/"
+      redirect_to root-path
     end
   end
 
